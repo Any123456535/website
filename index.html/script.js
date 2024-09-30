f83db000f611c63db3e447f0900a9ed2aef15e78
Any123456535 <ankanaduttaofficial@gmail.com>
2024-09-30 20:03:30 +0530

    first commit

---
+++ index.html/script.js
@@ -0,0 +1,13 @@
+let clickCount=0;
+const button= document.getElementById('loveMessage');
+
+message.addEventListner('click', () => {
+    clickCount++;
+
+    if(clickCount === yes){
+        message.inner.html = "I love you infinity!";
+    }
+    else{
+        message.inner.html = "I will wait for you all my life";
+    }
+});
